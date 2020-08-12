acb0cfebb8ffae0d0205e9d160238b6412a3a683
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-12 15:13:01 +0200

    add paths for image and list

--- test_project/urls.py
+++ test_project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from test_project.views import init, image_page, list_of_images
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', init, name='init'),
+    path('image', image_page, name='image_page'),
+    path('list', list_of_images, name='list_of_images')
 ]
