6400e23f21674fe6e2aeb742369778c5b44a613a
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-13 14:37:09 +0200

    fix paths

--- test_project/urls.py
+++ test_project/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from test_project.views import init, image_page, list_of_images
+from test_project.views import init, image_page, add_new_images
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', init, name='init'),
-    path('image', image_page, name='image_page'),
-    path('list', list_of_images, name='list_of_images')
+    path('image_page', image_page, name='image_page'),
+    path('add_new_images', add_new_images, name='add_new_image'),
+    path('', init, name='init')
 ]
