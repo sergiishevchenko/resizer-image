d7ab328df8e3479fc54b507673af021ea3431f58
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-15 09:15:55 +0200

    first migrations

--- test_project/models.py
+++ test_project/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=30)
-    cover = models.ImageField(upload_to='images/')
+    url = models.CharField(max_length=300)
+    files = models.FileField(upload_to='uploads/%Y/%m/%d/')
 
     def __str__(self):
         return self.title
