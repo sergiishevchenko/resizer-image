c4aa89ed91ed4d2b6f4aa05fafb043a319c6a682
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-15 09:24:52 +0200

    blank=True

--- test_project/models.py
+++ test_project/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
-    url = models.CharField(max_length=300)
-    files = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    url = models.CharField(max_length=300, blank=True)
+    files = models.FileField(upload_to='', blank=True)
 
     def __str__(self):
         return self.title
