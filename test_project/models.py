745e118136b318357b9ca9722268dd1502f08586
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-16 13:55:05 +0200

    add width and height

--- test_project/models.py
+++ test_project/models.py
@@ -5,5 +5,5 @@ class Post(models.Model):
     url = models.CharField(max_length=300, blank=True)
     files = models.FileField(upload_to='', blank=True)
 
-    def __str__(self):
-        return self.title
+    width = models.CharField(max_length=300, default='width')
+    height = models.CharField(max_length=300, default='height')
