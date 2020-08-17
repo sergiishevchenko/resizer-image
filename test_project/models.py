2c49fc625577ab676ab5582cb2032e4aab49b5a1
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-17 08:54:07 +0200

    fjx files

--- test_project/models.py
+++ test_project/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Post(models.Model):
     url = models.CharField(max_length=300, blank=True)
-    files = models.FileField(upload_to='', blank=True)
-
+    files = models.ImageField(upload_to='', height_field='height', width_field='width', blank=True)
     width = models.CharField(max_length=300, default='width')
     height = models.CharField(max_length=300, default='height')
