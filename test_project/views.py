36f8a14a7c93344c06bc8b8984084bd65323062c
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-20 15:09:12 +0200

    finish version

--- test_project/views.py
+++ test_project/views.py
@@ -22,38 +22,40 @@ def add_new_images(request):
 def image_page(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            if len(form['url'].value()) > 0:
-                urls = form['url'].value()
-                res = re.findall(r'(?<=\/)[^\/\?#]+(?=[^\/]*$)', urls)
-                files = res[0]
-                img_data = requests.get(urls).content
-                im = Image.open(BytesIO(img_data))
-                width = im.size[0]
-                height = im.size[1]
-                new_image = Post(url=urls, files=res[0], width=width, height=height)
-                new_image.save()
-                return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
-            else:
-                urls = ''
-                files = request.FILES['files'].name
-                image = Image.open(request.FILES['files'])
-                (width, height) = image.size
-                form.save()
-                return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
+        if (form['url'].value() or form['files'].value()) is not None:
+            if form.is_valid():
+                if len(form['url'].value()) > 0:
+                    urls = form['url'].value()
+                    res = re.findall(r'(?<=\/)[^\/\?#]+(?=[^\/]*$)', urls)
+                    files = res[0]
+                    img_data = requests.get(urls).content
+                    im = Image.open(BytesIO(img_data))
+                    width = im.size[0]
+                    height = im.size[1]
+                    new_image = Post(url=urls, files=res[0], width=width, height=height)
+                    new_image.save()
+                    return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
+                else:
+                    urls = ''
+                    files = request.FILES['files'].name
+                    image = Image.open(request.FILES['files'])
+                    (width, height) = image.size
+                    # new_image = Post(url=urls, files=files, width=width, height=height)
+                    form.save()
+                    return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
         else:
+            item = Post.objects.latest('id').id
             size_form = SizeForm(request.POST)
             if size_form.is_valid():
                 width = size_form['width'].value()
                 height = size_form['height'].value()
-                files = width + 'x' + height + '___' + str(files)
+                pre_image = Post.objects.get(id=item)
+                files = pre_image.files
+                urls = pre_image.url
+                # new_files = width + 'x' + height + '___' + str(files)
                 new_image = Post(url=urls, files=files, width=width, height=height)
                 new_image.save()
                 return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
-            else:
-                print('Error size!')
-    else:
-        print('Not request!')
 
 
 def image_page_from_list(request, id):
@@ -67,10 +69,8 @@ def image_page_from_list(request, id):
         if size_form.is_valid():
             width = size_form['width'].value()
             height = size_form['height'].value()
-            files = width + 'x' + height + '___' + str(files)
+            # new_files = width + 'x' + height + '___' + str(files)
             new_image = Post(url=urls, files=files, width=width, height=height)
             new_image.save()
             return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
-        else:
-            print('Error size!')
     return render(request, 'image_page.html', {'urls': urls, 'files': files, 'width': width, 'height': height})
